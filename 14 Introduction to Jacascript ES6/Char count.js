3746bac90d4224837a6f3a23f90965156ae74fa0
Troy Wilson <wilson.troy.anthony@gmail.com>
2023-09-15 08:06:17 -0400

    Update Char count.js

--- 14 Introduction to Jacascript ES6/Char count.js
+++ 14 Introduction to Jacascript ES6/Char count.js
@@ -2,6 +2,4 @@ const prompt = require("prompt-sync")();
 
 var text = prompt("Enter your text: ");
 
-
-
 console.log("You have written " + text.length + " characters, you have " + (140 - text.length) + " characters left.");

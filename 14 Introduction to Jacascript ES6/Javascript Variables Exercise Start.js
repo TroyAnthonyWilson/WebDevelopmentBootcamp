d6942ab3007a7983389e9443ba9099fdfcc9c56d
Troy Wilson <wilson.troy.anthony@gmail.com>
2023-09-15 07:07:18 -0400

    Update Javascript Variables Exercise Start.js

--- 14 Introduction to Jacascript ES6/Javascript Variables Exercise Start.js
+++ 14 Introduction to Jacascript ES6/Javascript Variables Exercise Start.js
@@ -1,9 +1,15 @@
+const prompt = require("prompt-sync")();
+
+Text();
+
 function Text() {
-    var a = "3";
-    var b = "8"
+  var a = "3";
+  var b = "8";
 
+  var c = b;
+  b = a;
+  a = c;
 
-
-    console.log('a is ' + a);
-    console.log('b is ' + b);
+  console.log("a is " + a);
+  console.log("b is " + b);
 }

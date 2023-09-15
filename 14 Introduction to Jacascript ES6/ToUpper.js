b893b3a88973e7563f3f363a3f39749658893974
Troy Wilson <wilson.troy.anthony@gmail.com>
2023-09-15 08:06:22 -0400

    Create ToUpper.js

---
+++ 14 Introduction to Jacascript ES6/ToUpper.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+
+var name = prompt("Enter your name: ");
+
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+console.log(name.slice(0,1).toUpperCase() + name.slice(1,name.length + 1).toLocaleLowerCase());
